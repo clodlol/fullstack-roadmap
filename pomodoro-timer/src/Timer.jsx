efe81e7eb128e21bc6306355bd125ed513bcfc0a
claude-3922 <aminabdulrehman532@gmail.com>
2026-07-08 11:19:47 +0500

    i'm moving on bruh

--- pomodoro-timer/src/Timer.jsx
+++ pomodoro-timer/src/Timer.jsx
@@ -3,50 +3,46 @@ import { useEffect, useState } from "react";
 export default function Timer() {
   const [timerHovered, setTimerHovered] = useState(false);
 
-  const [increasing, setIncreasing] = useState(false);
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [perc, setPerc] = useState(100);
+  const total = 15;
 
-  useEffect(() => {
-    if (perc >= 100) setIncreasing(false);
-    else if (perc <= 0) setIncreasing(true);
-  }, [perc]);
+  const [increasing, setIncreasing] = useState(false);
+  const [started, setStarted] = useState(false);
+  const [elapsed, setElapsed] = useState(total);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (!timerStarted) {
-        setPerc(100);
-        return;
-      }
+      if (!started) return setElapsed(total);
 
-      if (increasing) {
-        setPerc((p) => p + Math.floor(100 / 15));
-      } else {
-        setPerc((p) => p - Math.floor(100 / 30));
-      }
+      setElapsed((p) => {
+        if (p + 1 > total) setIncreasing(false);
+        if (p - 1 < 0) setIncreasing(true);
+
+        if (increasing) return p + 1;
+        else return p - 1;
+      });
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [timerStarted, increasing]);
+  }, [started, increasing]);
 
   return (
     <div
       id="timer-container"
       className="grid grid-cols-1 w-full min-h-50 bg-[#78A4CB] hover:cursor-pointer"
-      onClick={() => setTimerStarted((p) => !p)}
+      onClick={() => setStarted((p) => !p)}
       onMouseEnter={() => setTimerHovered(true)}
       onMouseLeave={() => setTimerHovered(false)}
     >
       <div
         className="transition-all [grid-area:1/1] min-h-50 bg-white/20"
         style={{
-          width: `${perc}%`,
+          width: `${increasing ? Math.round((elapsed / total) * 100) : 100 - Math.round((elapsed / total) * 100)}%`,
         }}
       ></div>
       <h1 className="[grid-area:1/1] h-full w-full flex items-center justify-center font-bold text-black/30 text-5xl">
         {timerHovered
-          ? `Click to ${timerStarted ? "Reset" : "Start"}`
-          : `${Math.round((perc * (increasing ? 15 : 30)) / 100)}`}{" "}
+          ? `Click to ${started ? "Reset" : "Start"}`
+          : `${elapsed}`}{" "}
       </h1>
     </div>
   );
